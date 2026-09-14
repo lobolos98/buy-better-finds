6a0be8e836899e7c8f7c62e5fc399b277c44abb4
lobolos98 <charliejonesart@gmail.com>
2026-09-13 20:11:01 -0400

    Use dependency-free sitemap endpoint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://buybetterfinds.com',
   trailingSlash: 'always',
-  integrations: [sitemap()],
   redirects: {
     '/categories/home/': '/categories/home-kitchen/',
     '/categories/kitchen/': '/categories/home-kitchen/',
