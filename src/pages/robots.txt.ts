16c289d578fc2c65fd1689792d7a4b4ec94a89d5
lobolos98 <charliejonesart@gmail.com>
2026-09-13 20:10:14 -0400

    Add robots.txt for search engine crawling

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from 'astro';
+
+const robotsTxt = `User-agent: *
+Allow: /
+
+Sitemap: https://buybetterfinds.com/sitemap-index.xml
+`;
+
+export const GET: APIRoute = () => {
+  return new Response(robotsTxt, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+};
