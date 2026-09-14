2523c47796e01616e87a0f84f67e518f1fe625dc
lobolos98 <charliejonesart@gmail.com>
2026-09-13 20:10:32 -0400

    Enable automatic sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://buybetterfinds.com',
   trailingSlash: 'always',
+  integrations: [sitemap()],
   redirects: {
     '/categories/home/': '/categories/home-kitchen/',
     '/categories/kitchen/': '/categories/home-kitchen/',
