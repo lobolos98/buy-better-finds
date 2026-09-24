39895e045740372385ce3fc6362c5f8f47e1f364
lobolos98 <charliejonesart@gmail.com>
2026-09-24 03:22:33 -0400

    Add verified manufacturer product image fallbacks

---
+++ src/data/manufacturer-images.js
@@ -0,0 +1,24 @@
+// Verified manufacturer-hosted product imagery.
+// Prefer Amazon Creators imagery when available; these entries are the manufacturer fallback.
+export const manufacturerImageOverrides = {
+  'anker-737-power-bank': {
+    image: 'https://cdn.shopify.com/s/files/1/0493/9834/9974/products/A1289011-Anker_737_Power_Bank_PowerCore_24K_1.png?v=1775967275&width=3840',
+    source: 'Anker'
+  },
+  'logitech-mx-keys-s': {
+    image: 'https://resource.logitech.com/w_544%2Ch_466%2Car_7%3A6%2Cc_pad%2Cq_auto%2Cf_auto%2Cdpr_1.0/d_transparent.gif/content/dam/logitech/en/products/keyboards/mx-keys-s/migration-assets-for-delorean-2025/gallery/mx-keys-s-top-view-graphite-us.png',
+    source: 'Logitech'
+  },
+  'dremel-4300-rotary-tool': {
+    image: 'https://shop.dremel.com/cdn/shop/files/dremel_4300-5-40_update_3000x3000_rendition_PNG.png?v=1746473729&width=416',
+    source: 'Dremel'
+  },
+  'kitchenaid-artisan-stand-mixer': {
+    image: 'https://kitchenaidus.vtexassets.com/arquivos/ids/159154/hero-KSM150PSBK.jpg?v=639198847141800000',
+    source: 'KitchenAid'
+  },
+  'crunchlabs-crunchinator': {
+    image: 'https://www.crunchlabs.com/cdn/shop/files/11601_CRUNCHLABS_CRUNCHNATOR_SINGLE_PK_F.png?v=1781203491&width=400',
+    source: 'CrunchLabs'
+  }
+};
